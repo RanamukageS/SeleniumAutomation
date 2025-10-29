7f40f94c04b30d912b8ac33c597c002a2876b1e1
shashikar1 <shashikaranamuka90@gmail.com>
2025-10-29 14:19:39 +1100

    8th Commit

--- src/test/java/tests/PIMTests.java
+++ src/test/java/tests/PIMTests.java
@@ -17,29 +17,29 @@ import java.util.concurrent.TimeUnit;
 @Listeners({utils.ExtentTestNGListner.class, utils.TestListner.class })
 public class PIMTests extends BaseTest {
 
-@Test
-    public void addEmployeetoPIM(){
-        adminPageObjects.enterRole();
-        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
-        pim.clickPIM();
-        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
-        pim.PIMSelectedTab();
-        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
-        pim.clickAddEmployee();
-        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("oxd-form-loader")));
-        pim.clickUploadProPic().click();
-        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
-        String desktopIMGPath = System.getProperty("user.home") + "/Desktop/image.jpeg";
-        File file  = new File(desktopIMGPath);
-        if(!file.exists()){
-            throw new RuntimeException("file not found" + file.getAbsolutePath());
-        }
-        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
-        pim.clickUploadProPic().sendKeys(file.getAbsolutePath());
-
-    }
+//@Test
+//    public void addEmployeetoPIM(){
+//        adminPageObjects.enterRole();
+//        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+//        pim.clickPIM();
+//        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+//        pim.PIMSelectedTab();
+//        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+//        pim.clickAddEmployee();
+//        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("oxd-form-loader")));
+//        pim.clickUploadProPic().click();
+//        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
+//        String desktopIMGPath = System.getProperty("user.home") + "/Desktop/image.jpeg";
+//        File file  = new File(desktopIMGPath);
+//        if(!file.exists()){
+//            throw new RuntimeException("file not found" + file.getAbsolutePath());
+//        }
+//        driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+//        pim.clickUploadProPic().sendKeys(file.getAbsolutePath());
+//
+//    }
 
 @Test(retryAnalyzer = RetryAnalyzer.class)
     public void addEmpDetails(){
